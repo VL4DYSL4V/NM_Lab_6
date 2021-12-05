6cae9b0e1382699b50a3191fa2d115f89a55b45e
VladislavArzamastsev <arzik66677@gmail.com>
2021-12-05 19:47:56 +0200

    Initial commit

---
+++ src/main/java/command/RunCommand.java
@@ -0,0 +1,12 @@
+package command;
+
+import framework.command.DefaultRunCommand;
+
+public class RunCommand extends DefaultRunCommand {
+
+    @Override
+    public void execute(String[] strings) {
+
+    }
+
+}
