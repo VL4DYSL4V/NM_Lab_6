47e8ea22eb4df10cab269d3dc092377629d04ecb
VladyslavArzamastsev <arzik66677@gmail.com>
2021-12-05 21:26:29 +0200

    Done with the lab

--- src/main/java/command/RunCommand.java
+++ src/main/java/command/RunCommand.java
@@ -1,12 +1,30 @@
 package command;
 
 import framework.command.DefaultRunCommand;
+import framework.utils.ConsoleUtils;
+import org.apache.commons.math3.analysis.BivariateFunction;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 public class RunCommand extends DefaultRunCommand {
 
     @Override
     public void execute(String[] strings) {
+        BivariateFunction function = (BivariateFunction) applicationState.getVariable("function");
+        Vector2D point = (Vector2D) applicationState.getVariable("point");
+        double xN = (Double) applicationState.getVariable("xN");
+        double n = (Double) applicationState.getVariable("n");
 
+        double step = (xN - point.getX()) / n;
+        ConsoleUtils.println(String.format("%n  X\t\t  Y"));
+        ConsoleUtils.println("----------------");
+
+        for(int i = 0; i < n; i++) {
+            double yN = point.getY() + step * function.value(point.getX(), point.getY());
+            ConsoleUtils.println(String.format("%.3f\t%.3f", point.getX(), point.getY()));
+            point = new Vector2D(point.getX() + step, yN);
+        }
+
+        ConsoleUtils.println(String.format("%nAt x = %.3f y = %.3f", point.getX(), point.getY()));
     }
 
 }
--- src/main/java/state/LaboratoryState.java
+++ src/main/java/state/LaboratoryState.java
@@ -2,7 +2,28 @@ package state;
 
 import framework.state.AbstractApplicationState;
 import lombok.Getter;
+import org.apache.commons.math3.analysis.BivariateFunction;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.apache.commons.math3.util.FastMath;
 
 @Getter
 public class LaboratoryState extends AbstractApplicationState {
+
+    private final BivariateFunction function = (x, y) -> x + y;
+
+    private final Vector2D vector2D = new Vector2D(0, 1);
+
+    //    calculation point
+    private final double xN = 0.5;
+
+    //    amount of steps
+    private final double n = 10;
+
+    @Override
+    protected void initVariableNameToGettersMap() {
+        this.variableNameToGetter.put("point", this::getVector2D);
+        this.variableNameToGetter.put("function", this::getFunction);
+        this.variableNameToGetter.put("xN", this::getXN);
+        this.variableNameToGetter.put("n", this::getN);
+    }
 }
